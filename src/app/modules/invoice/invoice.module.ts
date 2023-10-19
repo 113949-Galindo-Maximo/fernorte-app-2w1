3adebf8b8f504cba57391b8dbdceaeec3f75663b
113949-Galindo-Maximo <113949@tecnicatura.frc.utn.edu.ar>
2023-10-19 15:44:57 -0300

    fix conflicts

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< Updated upstream
 import { DeleteFacturaComponent } from './components/delete-factura/delete-factura.component';
-
-@NgModule({
-  declarations: [HomeComponent, DeleteFacturaComponent],
-=======
 import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
-
 @NgModule({
-  declarations: [HomeComponent, InvoiceDetailComponent],
->>>>>>> Stashed changes
+  declarations: [HomeComponent, InvoiceDetailComponent,DeleteFacturaComponent],
   providers: [],
   imports: [CommonModule],
   exports: [HomeComponent],
