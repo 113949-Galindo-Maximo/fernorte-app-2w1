61ec45da4316697d92dadd3e1eaf2dd1189f483b
113949-Galindo-Maximo <113949@tecnicatura.frc.utn.edu.ar>
2023-10-19 22:03:11 -0300

    borro componentes que no se usan

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
-import { DeleteFacturaComponent } from './components/delete-factura/delete-factura.component';
-import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
 import { RegistrarFacturaComponent } from './components/registrar-factura/registrar-factura.component';
 import { GestionFacturaComponent } from './components/gestion-factura/gestion-factura.component';
 import { PendingOrdersComponent } from './components/pending-orders/pending-orders.component';
@@ -11,9 +9,7 @@ import { FormPagoComponent } from './components/form-pago/form-pago.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent,
-    InvoiceDetailComponent,
-    DeleteFacturaComponent,
+  declarations: [HomeComponent,  
     RegistrarFacturaComponent,
     PendingOrdersComponent,
     GestionFacturaComponent,
