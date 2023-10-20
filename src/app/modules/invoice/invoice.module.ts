2ee874f292b82f502150e6449256babf7584daa0
113949-Galindo-Maximo <113949@tecnicatura.frc.utn.edu.ar>
2023-10-19 21:55:46 -0300

    arreglo errores

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -6,12 +6,22 @@ import { InvoiceDetailComponent } from './components/invoice-detail/invoice-deta
 import { RegistrarFacturaComponent } from './components/registrar-factura/registrar-factura.component';
 import { GestionFacturaComponent } from './components/gestion-factura/gestion-factura.component';
 import { PendingOrdersComponent } from './components/pending-orders/pending-orders.component';
+import { RegistrarPagoComponent } from './components/registrar-pago/registrar-pago.component';
+import { FormPagoComponent } from './components/form-pago/form-pago.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent, InvoiceDetailComponent,DeleteFacturaComponent, RegistrarFacturaComponent, PendingOrdersComponent,GestionFacturaComponent],
+  declarations: [HomeComponent,
+    InvoiceDetailComponent,
+    DeleteFacturaComponent,
+    RegistrarFacturaComponent,
+    PendingOrdersComponent,
+    GestionFacturaComponent,
+    RegistrarPagoComponent,
+    FormPagoComponent],
 
   providers: [],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [HomeComponent],
 })
-export class InvoiceModule {}
+export class InvoiceModule { }
